50243a29f7fd1cbffa9ba97f0d2718a9bfd87c06
zrm99 <36284384+zrm99@users.noreply.github.com>
2020-08-03 19:06:12 -0700

    Init

---
+++ README.md
@@ -0,0 +1,25 @@
+# google-image-terminal
+
+google-image-terminal takes in input from the Node.js command prompt and cycles
+through several google images randomly based on the input and renders them with
+terminal-image. A link is posted after the image has rendered to show the source.
+
+<h2> Installation instructions </h2>
+
+<ol>
+  <li>Download the main directory</li>
+  <li>Open the Node.js command prompt</li>
+  <li>Navigate to the inside of the directory.</li>
+  <li>Run 'npm install'</li>
+  <li>Run 'node start.js'</li>
+  <li>Type in your desired search term</li>
+</ol>
+
+<h2> Settings </h2>
+
+<h4> Image limit </h4>
+There is a limit of ten google images to keep the relevancy of the search term, to
+change the limit, simply change the variable, 'imageLimit' in start.js from
+'10' to the desired amount.
+
+![Terminal search](https://user-images.githubusercontent.com/36284384/89242526-a2c7f900-d5b6-11ea-9500-3da321b6ecbb.JPG)
---
+++ start.js
@@ -0,0 +1,41 @@
+const readline = require("readline");
+const terminalImage = require('terminal-image');
+const got = require('got');
+const gis = require('g-i-s');
+
+var imageLimit = 10;
+
+// The GoogleImage function takes in a search term for Google images.
+// 'gis' takes in that search term and collects the URLS in a JSON format.
+// The JSON results are cycled through with a random number of up to ten
+// and the URL is inserted into 'got' and 'terminalImage' renders the image
+// onto the terminal.
+function GoogleImage(image) {
+  gis(image, logResults);
+  function logResults(error, results) {
+    if (error) {
+      console.log("Image error");
+    }
+    else {
+      (async () => {
+        var num = Math.floor(Math.random() * (imageLimit + 1));
+        const body = await got(results[num].url).buffer();
+        console.log(await terminalImage.buffer(body, {width: '50%', height: '50%'}));
+        console.log(results[num].url + '\n');
+      })();
+    }
+  }
+}
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.setPrompt("> ");
+rl.prompt();
+rl.on("line", function(cmd) {
+  GoogleImage(cmd)
+}).on("close", function() {
+  process.exit(0);
+});
